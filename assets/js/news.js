071b039c00f43372deae5b0d5551d2849b899ce9
Mohamed Elzohary <MohammadBahaa_2001mb@outlook.com>
2021-03-29 07:20:13 +0200

    edited: news theme, page struct

---
+++ assets/js/news.js
@@ -0,0 +1,14 @@
+// this script for the v2.0 of the news section theme
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
