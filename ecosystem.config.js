69512a0423007d203e0d2fc3f69ffb1f5ab7f111
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2021-12-04 17:05:58 +0200

    added: pm2 ecosystem | updated: icon

---
+++ ecosystem.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  apps: [
+    {
+      name: "server",
+      cwd: "/server",
+      script: "./app.js",
+      instances: 1,
+      out_file: "/logs/pm2.out.log",
+      error_file: "/logs/pm2.err.log",
+      node_args: ["--max_old_space_size=256"],
+      max_memory_restart: '256M',
+      env_production: {
+        NODE_ENV: "production"
+      },
+      env_development: {
+        NODE_ENV: "development"
+      },
+    },
+    {
+      name: "admin",
+      cwd: '/admin/v2',
+      script: 'npm',
+      args: "start",
+      instances: 1,
+      out_file: "/logs/pm2.out.log",
+      error_file: "/logs/pm2.err.log",
+      node_args: ["--max_old_space_size=256"],
+      max_memory_restart: '256M',
+      env_production: {
+        NODE_ENV: "production"
+      },
+      env_development: {
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "chat",
+      cwd: "/chat",
+      script: "./app.js",
+      instances: 1,
+      out_file: "/logs/pm2.out.log",
+      error_file: "/logs/pm2.err.log",
+      node_args: ["--max_old_space_size=128"],
+      max_memory_restart: '128M',
+      env_production: {
+        NODE_ENV: "production"
+      },
+      env_development: {
+        NODE_ENV: "development"
+      }
+    },
+  ]
+}
