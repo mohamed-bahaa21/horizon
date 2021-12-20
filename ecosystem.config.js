acddd2efdedf58ef32fceb02b3d44ca1efa2b8ea
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2021-12-21 00:41:07 +0200

    updated: pm2 max_restarts, restart_delay

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,8 @@ module.exports = {
       error_file: "/logs/pm2.err.log",
       node_args: ["--max_old_space_size=256"],
       max_memory_restart: '256M',
+      max_restarts: '10',
+      restart_delay: '3000',
       env_production: {
         NODE_ENV: "production"
       },
@@ -26,6 +28,8 @@ module.exports = {
       error_file: "/logs/pm2.err.log",
       node_args: ["--max_old_space_size=256"],
       max_memory_restart: '256M',
+      max_restarts: '3',
+      restart_delay: '3000',
       env_production: {
         NODE_ENV: "production"
       },
@@ -42,6 +46,8 @@ module.exports = {
       error_file: "/logs/pm2.err.log",
       node_args: ["--max_old_space_size=128"],
       max_memory_restart: '128M',
+      max_restarts: '10',
+      restart_delay: '3000',
       env_production: {
         NODE_ENV: "production"
       },
